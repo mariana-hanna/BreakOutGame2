4970945d8c2e462b858f68f236e05d62972c285a
mariana-hanna <159145980+mariana-hanna@users.noreply.github.com>
2024-04-24 13:45:31 +0200

    Update score.cpp

--- score.cpp
+++ score.cpp
@@ -1,16 +1,24 @@
 #include "Score.h"
+#include "Game.h"
 #include <QFont>
 
 Score::Score(QGraphicsTextItem *parent): QGraphicsTextItem(parent){
     score = 0;
     setPlainText(QString("Score: ") + QString::number(score));
-    setDefaultTextColor(Qt::blue);
+    setDefaultTextColor(Qt::black);
     setFont(QFont("times", 16));
 }
 
 void Score::increase(){
+
+    if(game->gameOver) {
+        return;
+    }
+
     score++;
     setPlainText(QString("Score: ") + QString::number(score));
+
+
 }
 
 int Score::getScore(){
