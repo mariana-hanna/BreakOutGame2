4f7c4e2ccd31c804c60d69b2c5db55e12646471b
mariana-hanna <159145980+mariana-hanna@users.noreply.github.com>
2024-04-24 13:43:11 +0200

    Update game.h

--- game.h
+++ game.h
@@ -5,18 +5,33 @@
 #include <QGraphicsScene>
 #include "score.h"
 #include "health.h"
+#include "Button.h"
 
 class Game: public QGraphicsView{
+
+   Q_OBJECT
 public:
     Game(QWidget* parent=NULL);
     Score * score;
 
     Health * health;
 
-    void start();
-    void Block_Display();
 
+    void Row_of_Blocks(int n);
+    void Block_Display();
+    void GameOver();
+    void displayGameOver(QString text);
+ bool gameOver;
     QGraphicsScene* scene;
+
+public slots:
+    void start();
+   void RestartGame();
+
+private:
+   void showPanel(int x, int y, int width, int height, QColor color, double opacity);
+    Button* playAgain;
+    Button* exit;
 
 };
 
