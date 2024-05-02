0b3038a26f399006b076f23ea1eb4a488ad53a0a
SaraHossameldin <159119664+SaraHossameldin@users.noreply.github.com>
2024-05-03 00:11:40 +0300

    Updated Button.cpp

--- Button.cpp
+++ Button.cpp
@@ -1,3 +1,43 @@
-#include "button.h"
+#include "Button.h"
+#include <QGraphicsTextItem>
+#include <QBrush>
+#include <QGraphicsItem>
 
-Button::Button() {}
+Button::Button(QString name, QGraphicsItem *parent): QGraphicsRectItem(parent){
+    // drawing the rect
+    setRect(0,0,200,50);
+    QBrush brush;
+    brush.setStyle(Qt::SolidPattern);
+    brush.setColor(Qt::darkCyan);
+    setBrush(brush);
+
+    // drawing the text
+    text = new QGraphicsTextItem(name,this);
+    int xPos = rect().width()/2 - text->boundingRect().width()/2;
+    int yPos = rect().height()/2 - text->boundingRect().height()/2;
+    text->setPos(xPos,yPos);
+
+    // allow responding to hover events
+    setAcceptHoverEvents(true);
+}
+
+void Button::mousePressEvent(QGraphicsSceneMouseEvent *event){
+
+    emit clicked();
+}
+
+void Button::hoverEnterEvent(QGraphicsSceneHoverEvent *event){
+    // change color to cyan
+    QBrush brush;
+    brush.setStyle(Qt::SolidPattern);
+    brush.setColor(Qt::cyan);
+    setBrush(brush);
+}
+
+void Button::hoverLeaveEvent(QGraphicsSceneHoverEvent *event){
+    // change color to dark cyan
+    QBrush brush;
+    brush.setStyle(Qt::SolidPattern);
+    brush.setColor(Qt::darkCyan);
+    setBrush(brush);
+}
