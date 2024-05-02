b49a61a56b8b04de49c13cada1cc16d96f772356
SaraHossameldin <159119664+SaraHossameldin@users.noreply.github.com>
2024-05-03 00:12:04 +0300

    Update Button.h

--- Button.h
+++ Button.h
@@ -1,10 +1,24 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
-class Button
-{
+#include<QGraphicsItem>
+#include <QGraphicsRectItem>
+#include <QGraphicsSceneMouseEvent>
+
+class Button:public QObject, public QGraphicsRectItem{
+    Q_OBJECT
 public:
-    Button();
+    // constructors
+    Button(QString name, QGraphicsItem* parent=NULL);
+
+    // public methods (events)
+    void mousePressEvent(QGraphicsSceneMouseEvent *event);
+    void hoverEnterEvent(QGraphicsSceneHoverEvent *event);
+    void hoverLeaveEvent(QGraphicsSceneHoverEvent *event);
+signals:
+    void clicked();
+private:
+    QGraphicsTextItem* text;
 };
 
 #endif // BUTTON_H
